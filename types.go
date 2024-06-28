c7db4bf125798af7844b0b199aabf759d563de44
Achal Venkatesh <thorsthorfinn2003@gmail.com>
2024-06-28 21:31:46 +0530

    Added types.go file

---
+++ types.go
@@ -0,0 +1,44 @@
+package main
+
+const (
+	alphaVantageURL    = "https://www.alphavantage.co/query"
+	nseURL             = "https://www.nseindia.com/api/quote-equity?symbol="
+)
+
+type Users struct{
+	Email string `form:"email"`
+	Username string `form: "username"`
+	Password string `form: "password"`
+	CPassword string `form: "cpassword"`
+	PhoneNumber string `form: "phoneNumber"`
+}
+
+type Admin struct{
+	Username string `form:"email"`
+	Password string `form:"password"`
+}
+
+type Posts struct{
+	Stock_name string `form:"stock_name"`
+	Current_price string `form:"buying_price"`
+	Target_price string `form:"target_price"`
+	Comments string `form:"comments"`
+	Exchange string `form:"exchange"`
+}
+
+type News struct{
+	New string `form:"news"`
+	Link string `form:"links"`
+}
+
+type BSEStockResponse struct {
+	GlobalQuote struct {
+		Price string `json:"05. price"`
+	} `json:"Global Quote"`
+}
+
+type NSEStockResponse struct {
+	PriceInfo struct {
+		LastPrice float64 `json:"lastPrice"`
+	} `json:"priceInfo"`
+}
